982303e166d8f94fed243b6e45d68e66cde2d2df
stevenEno <steven.e.eno@gmail.com>
2022-03-30 18:49:31 -0700

    Added DB connection error handling

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
-from DBcm import UseDatabase, ConnectionError
+from DBcm import UseDatabase, ConnectionError, CredentialsError
 from checker import check_logged_in
 
 app = Flask(__name__)
@@ -59,6 +59,8 @@ def view_the_log() -> 'html':
         return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents)
     except ConnectionError as err:
         print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
     except Exception as err:
         print('Something went wrong:', str(err))
     return 'Error'
